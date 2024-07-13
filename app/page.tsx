49ca92b2a84be775c3147364cfcd4d2258c5f7dd
Mark Andrey <markandreydelacruz@ymail.com>
2024-07-13 16:30:37 +0800

    put clock and black screen apps in Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,22 @@
+import Link from "next/link";
 export default function Root() {
   return (
-    <main className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-3xl font-bold mb-4">Welcome to the Home Page!</h1>
-      <p className="text-lg text-gray-600">
-        This is the home page of your application.
-      </p>
+    <main className="container mx-auto h-screen">
+      <div className="text-2xl uppercase font-bold py-4">All Apps</div>
+      <ul className="flex flex-col">
+        <li className="p-1 w-fit">
+          <text className="font-bold">MADC Clock: </text>
+          <a href="https://madc-clock.vercel.app" target="_blank">
+            https://madc-clock.vercel.app
+          </a>
+        </li>
+        <li className="p-1 w-fit">
+        <text className="font-bold">Black Screen: </text>
+          <a href="https://blackscreenapp.vercel.app" target="_blank">
+            https://blackscreenapp.vercel.app
+          </a>
+        </li>
+      </ul>
     </main>
   );
 }
-
-
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header className="bg-gray-200 py-4 px-8 top-0 w-full">
       <nav className="container mx-auto flex justify-between items-center">
-        <h1 className="text-2xl font-bold">MADC</h1>
+        <h1 className="text-3xl font-bold"><Link href="/">MADC</Link></h1>
         <ul className="flex space-x-4">
           <li>
             <Link href="/">Home</Link>
